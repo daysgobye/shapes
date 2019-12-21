411a23b5c06343971f85d4d991f94f3c96272e74
Cole Smith <smithcole@protonmail.com>
2019-12-21 01:40:35 -0800

    Updates

---
+++ precache-manifest.4206096de6690e57f2f187a70ae54d02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a768876917871a7454500651c08b6a5b",
+    "url": "/shapes/index.html"
+  },
+  {
+    "revision": "8cdaa6ec0012f491b9ae",
+    "url": "/shapes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a7952cb80c97eb66e9c0",
+    "url": "/shapes/static/js/2.dc1d20d1.chunk.js"
+  },
+  {
+    "revision": "29c8d19450d117745c250b1131e2dada",
+    "url": "/shapes/static/js/2.dc1d20d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cdaa6ec0012f491b9ae",
+    "url": "/shapes/static/js/main.db6c3a67.chunk.js"
+  },
+  {
+    "revision": "aa828fd134ea82ba6bea",
+    "url": "/shapes/static/js/runtime-main.e221b9fb.js"
+  },
+  {
+    "revision": "e5738053468321ecbf4519908406e881",
+    "url": "/shapes/static/media/ic.e5738053.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shapes/static/media/logo.5d5d9eef.svg"
+  }
+]);
